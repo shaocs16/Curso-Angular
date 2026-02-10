0ca26a5ecf27b75246abd073359aedf04bb118c7
xabierscs <xabierscs@ext.inditex.com>
2026-02-10 17:55:55 +0100

    computed

--- 02-bases/src/app/pages/hero/hero-page.ts
+++ 02-bases/src/app/pages/hero/hero-page.ts
@@ -1,16 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { UpperCasePipe } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
-  templateUrl: './hero-page.html'
+  templateUrl: './hero-page.html',
+  imports: [UpperCasePipe]
 })
 
 export class HeroPageComponent {
   name = signal("Ironman")
   age = signal(47)
 
-  getHeroDescription() {
-    return `${this.name()} - ${this.age()}`
-  }
+  heroDescription = computed(() => {
+    const description = `${this.name()} - ${this.age()}`
+    return description
+  })
+
+  capitalizedName = computed(() => this.name().toUpperCase())
 
   changeHero() {
     this.name.set("Spiderman")
