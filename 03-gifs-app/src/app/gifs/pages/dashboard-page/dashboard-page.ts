41fb171428d9005d6654d40a9b0f45cf8e9516ba
xabierscs <xabierscs@ext.inditex.com>
2026-02-11 18:09:47 +0100

    leccion-71 side components

--- 03-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.ts
+++ 03-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
+import { SideMenuHeader } from "../../components/side-menu/side-menu-header/side-menu-header";
+import { SideMenuOptions } from "../../components/side-menu/side-menu-options/side-menu-options";
+import { SideMenu } from "../../components/side-menu/side-menu";
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideMenu],
   templateUrl: './dashboard-page.html'
 })
 export default class DashboardPage { }
