ef7a2d1a94ad4c8df16d16ff26817c5849151801
xabierscs <xabierscs@ext.inditex.com>
2026-02-11 18:54:09 +0100

    leccion-73 env y paths alias

--- 03-gifs-app/src/app/gifs/components/side-menu/side-menu-header/side-menu-header.ts
+++ 03-gifs-app/src/app/gifs/components/side-menu/side-menu-header/side-menu-header.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { environment } from '@environments/environment';
+//import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'gifs-side-menu-header',
   imports: [],
   templateUrl: './side-menu-header.html'
 })
-export class SideMenuHeader { }
+export class SideMenuHeader {
+
+  envs = environment
+
+}
