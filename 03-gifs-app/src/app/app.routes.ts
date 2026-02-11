639e132313829fe5ef676b6408d10a7b75326b1e
xabierscs <xabierscs@ext.inditex.com>
2026-02-11 17:12:24 +0100

    leccion-90 rutas hijas

--- 03-gifs-app/src/app/app.routes.ts
+++ 03-gifs-app/src/app/app.routes.ts
@@ -4,10 +4,22 @@ export const routes: Routes = [
 
   {
     path: 'dashboard',
-    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page')
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page'),
+    children: [
+      {
+      path: 'trending',
+      loadComponent: () => import('./gifs/pages/trending-page/trending-page')
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('./gifs/pages/search-page/search-page')
+      },
+      {
+        path: '**',
+        redirectTo: 'trending'
+      },
+    ]
   },
-
-
   {
     path: '**',
     redirectTo: 'dashboard'
--- 03-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.ts
+++ 03-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './dashboard-page.html'
 })
 export default class DashboardPage { }
