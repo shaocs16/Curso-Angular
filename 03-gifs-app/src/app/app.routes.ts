91cc1edc8676da4a0669d1b6c5ac96293413dcae
xabierscs <xabierscs@ext.inditex.com>
2026-02-11 16:56:45 +0100

    leccion-69

--- 03-gifs-app/src/app/app.routes.ts
+++ 03-gifs-app/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page')
+  },
+
+
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+
+];
