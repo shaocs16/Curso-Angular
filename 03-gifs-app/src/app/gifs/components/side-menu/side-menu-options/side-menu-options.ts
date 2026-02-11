50e1c172ee8e8f31f5020289708477fc26ab8bd1
xabierscs <xabierscs@ext.inditex.com>
2026-02-11 18:29:06 +0100

    leccion-72

--- 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
+++ 03-gifs-app/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.ts
@@ -1,8 +1,33 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOptions {
+  icon: string;
+  label: string;
+  subLabel: string;
+  route: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html',
 })
-export class SideMenuOptions { }
+export class SideMenuOptions {
+
+  menuOptions: MenuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search'
+    }
+  ]
+
+}
