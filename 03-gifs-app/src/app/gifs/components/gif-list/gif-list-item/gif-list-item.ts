5ef649a81250e90ea4d460846a4d22a7b0681b4d
xabierscs <xabierscs@ext.inditex.com>
2026-02-12 13:40:22 +0100

    leccion-75 input images

--- 03-gifs-app/src/app/gifs/components/gif-list/gif-list-item/gif-list-item.ts
+++ 03-gifs-app/src/app/gifs/components/gif-list/gif-list-item/gif-list-item.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'gif-list-item',
   imports: [],
   templateUrl: './gif-list-item.html'
 })
-export class GifListItem { }
+export class GifListItem {
+
+  imageUrl = input.required<string>()
+
+}
